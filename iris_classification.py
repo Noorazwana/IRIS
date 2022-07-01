d6bb586fa55b46b3959babce1fd136260e51c05b
Noorazwana <108342940+Noorazwana@users.noreply.github.com>
2022-07-01 13:51:59 +0800

    Update iris_classification.py

--- iris_classification.py
+++ iris_classification.py
@@ -29,7 +29,7 @@ st.write(df)
 #iris = github.load_iris()
 iris = pd.read_csv('IRIS.csv')
 st.write(iris)
-X = drop('species',axis=1)
+X = iris.drop('species',axis=1)
 Y = iris.species
 
 clf = RandomForestClassifier()
